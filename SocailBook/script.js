7e4083a966f73ea906d072a77863931c852ad7f8
Sanikasankapal <156881949+Sanikasankapal@users.noreply.github.com>
2024-01-16 22:09:38 -0800

    Add files via upload

---
+++ SocailBook/script.js
@@ -0,0 +1,6 @@
+var settingsmenu=document.querySelector(".settings-menu");
+vardarkbtn=document.getElementById("dark-btn")
+function settingMenuToggle(){
+    settingsmenu.classList.toggle("settings-menu-height");
+}
+
